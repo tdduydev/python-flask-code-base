9cceefff944c63a555ffe567b339d0f9ffe75528
TraiTNDev <traitn.dev@gmail.com>
2021-12-17 13:59:27 +0700

    Merge branch 'TraiNT' of https://github.com/tdduydev/python-flask-code-base into TraiNT

--- myapi/api/schemas/user.py
+++ myapi/api/schemas/user.py
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-from flask_marshmallow import Marshmallow
-from myapi.models import User
-from myapi.extensions import ma, db
-
-from marshmallow import fields ,validate
-
-class UserSchema(ma.SQLAlchemyAutoSchema):
-
-    id = ma.Int(dump_only=True)
-    password = ma.String(load_only=True, required=True)
-    email = fields.Email()
-    lastName = fields.String(validate=validate.Length(max=20))
-    firstName = fields.String(validate=validate.Length(max=20))
-    address = fields.String(validate=validate.Length(max=80))
-    phoneNumber = fields.String(validate=validate.Length(max=12))
-
-    class Meta:
-        model = User
-        sqla_session = db.session
-        load_instance = True
-        exclude = ("_password",)
-=======
 from myapi.models import User
 from myapi.extensions import ma, db
 
@@ -42,4 +19,3 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
         sqla_session = db.session
         load_instance = True
         exclude = ("_password",)
->>>>>>> main
--- myapi/app.py
+++ myapi/app.py
@@ -1,68 +1,3 @@
-<<<<<<< HEAD
-from flask import Flask
-from myapi import api
-from myapi import auth
-from myapi import manage
-from myapi.extensions import apispec
-from myapi.extensions import db
-from myapi.extensions import jwt
-from myapi.extensions import migrate
-from flask_cors import CORS
-
-
-def create_app(testing=False):
-    """Application factory, used to create application"""
-    app = Flask("myapi")
-    app.config.from_object("myapi.config")
-    CORS(app)
-
-    if testing is True:
-        app.config["TESTING"] = True
-
-    configure_extensions(app)
-    configure_cli(app)
-    configure_apispec(app)
-    register_blueprints(app)
-
-    return app
-
-
-def configure_extensions(app):
-    """Configure flask extensions"""
-    db.init_app(app)
-    jwt.init_app(app)
-    migrate.init_app(app, db)
-
-
-def configure_cli(app):
-    """Configure Flask 2.0's cli for easy entity management"""
-    app.cli.add_command(manage.init)
-
-
-def configure_apispec(app):
-    """Configure APISpec for swagger support"""
-    apispec.init_app(app, security=[{"jwt": []}])
-    apispec.spec.components.security_scheme(
-        "jwt", {"type": "http", "scheme": "bearer", "bearerFormat": "JWT"}
-    )
-    apispec.spec.components.schema(
-        "PaginatedResult",
-        {
-            "properties": {
-                "total": {"type": "integer"},
-                "pages": {"type": "integer"},
-                "next": {"type": "string"},
-                "prev": {"type": "string"},
-            }
-        },
-    )
-
-
-def register_blueprints(app):
-    """Register all blueprints for application"""
-    app.register_blueprint(auth.views.blueprint)
-    app.register_blueprint(api.views.blueprint)
-=======
 from flask import Flask
 from myapi import api
 from myapi import auth
@@ -130,4 +65,3 @@ def register_blueprints(app):
     """Register all blueprints for application"""
     app.register_blueprint(auth.views.blueprint)
     app.register_blueprint(api.views.blueprint)
->>>>>>> main
